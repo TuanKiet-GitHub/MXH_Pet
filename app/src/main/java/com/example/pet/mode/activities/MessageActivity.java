e574cea55d752e9c907280dc3347252252f8c65e
TuanKiet-GitHub <tuankiet11a3@gmail.com>
2021-05-19 11:42:38 +0700

    Message Versison 01

---
+++ app/src/main/java/com/example/pet/mode/activities/MessageActivity.java
@@ -0,0 +1,15 @@
+package com.example.pet.mode.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pet.R;
+
+public class MessageActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+    }
+}
