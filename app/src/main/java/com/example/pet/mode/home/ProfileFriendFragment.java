9c5af24d7ed351d2bed1f990bf1025a3b620a42a
RakiticVo <thienb1704852@gmail.com>
2021-05-22 10:17:00 +0700

    setting_button_send_request

--- app/src/main/java/com/example/pet/mode/home/ProfileFriendFragment.java
+++ app/src/main/java/com/example/pet/mode/home/ProfileFriendFragment.java
@@ -16,11 +16,14 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.pet.R;
 import com.example.pet.mode.activities.SearchFriendActivity;
 import com.example.pet.mode.models.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -28,6 +31,9 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.concurrent.Executor;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -37,9 +43,15 @@ import de.hdodenhof.circleimageview.CircleImageView;
  * create an instance of this fragment.
  */
 public class ProfileFriendFragment extends Fragment {
-    private DatabaseReference friendReference;
+    private DatabaseReference friendReference, requestRef, friendRequestRef;
     CircleImageView circleImageView;
     TextView userName, nickName, gender, DOB, address, phone;
+    Button send, decline;
+    ImageButton back;
+    String value;
+    SharedPreferences sharedPreferences;
+    String token;
+    String currentState = "nothing_happen";
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -86,6 +98,9 @@ public class ProfileFriendFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile_friend, container, false);
+        value = this.getArguments().getString("id");
+        sharedPreferences = view.getContext().getSharedPreferences("login", Context.MODE_PRIVATE);
+        token = sharedPreferences.getString("token", "1");
 
         circleImageView = view.findViewById(R.id.avatar_friend);
         userName = view.findViewById(R.id.tv_username_friend);
@@ -95,7 +110,11 @@ public class ProfileFriendFragment extends Fragment {
         address = view.findViewById(R.id.tv_address_friend);
         phone = view.findViewById(R.id.tv_phoneNumber_friend);
 
-        String value = this.getArguments().getString("id");
+        send = view.findViewById(R.id.SendRequest_friend);
+        decline = view.findViewById(R.id.DeclineRequest_friend);
+        back = view.findViewById(R.id.btn_back);
+
+
         Log.e("TAG", ""+ value );
 
         if (!value.isEmpty()) {
@@ -124,23 +143,142 @@ public class ProfileFriendFragment extends Fragment {
         }
 
         // Return
-        ImageButton btn_back = view.findViewById(R.id.btn_back);
-        btn_back.setOnClickListener(new View.OnClickListener() {
+
+        back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(v.getContext(), SearchFriendActivity.class));
             }
         });
 
+        requestRef = FirebaseDatabase.getInstance().getReference().child("Users").child(""+token).child("Requests");
+        friendRequestRef = FirebaseDatabase.getInstance().getReference().child("Users").child(""+value).child("Friend_Request");
+
         // Send Request
-        Button send = view.findViewById(R.id.SendRequest);
-        btn_back.setOnClickListener(new View.OnClickListener() {
+
+        send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(v.getContext(), SearchFriendActivity.class));
+                perfromAction(value);
+            }
+        });
+
+        // Declide Request
+
+        decline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
 
         return view;
+    }
+
+    // Thực hiện kết bạn
+
+    private void perfromAction(String value) {
+        if (currentState.equals("nothing_happen")){
+            HashMap hashMap = new HashMap();
+            hashMap.put("status", "Pending");
+            hashMap.put("userId", value);
+            requestRef.child(value).updateChildren(hashMap).addOnCompleteListener(new OnCompleteListener() {
+                @Override
+                public void onComplete(@NonNull @NotNull Task task) {
+                    if (task.isSuccessful()){
+                        HashMap hashMap = new HashMap();
+                        hashMap.put("status", "Friend_Request");
+                        hashMap.put("userId", token);
+                        friendRequestRef.child(token).updateChildren(hashMap).addOnCompleteListener(new OnCompleteListener() {
+                            @Override
+                            public void onComplete(@NonNull @NotNull Task task) {
+                                if (task.isSuccessful()){
+                                    Toast.makeText(getContext(), "You just send a friend request", Toast.LENGTH_SHORT).show();
+                                    decline.setVisibility(View.GONE);
+                                    currentState = "I_sent_pending";
+                                    send.setText("Cancel Friend Request");
+                                }
+                                else {
+                                    Toast.makeText(getContext(), "Can't send \n" + task.getException() , Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }
+                    else {
+                        Toast.makeText(getContext(), "Can't send \n" + task.getException() , Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+        if (currentState.equals("I_sent_pending") || currentState.equals("I_sent_decline")){
+            requestRef.child(value).removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull @NotNull Task<Void> task) {
+                    if (task.isSuccessful()){
+                        friendRequestRef.child(token).removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull @NotNull Task<Void> task) {
+                                if (task.isSuccessful()){
+                                    Toast.makeText(getContext(), "You have canceled a friend request", Toast.LENGTH_SHORT).show();
+                                    decline.setVisibility(View.GONE);
+                                    currentState = "nothing_happen";
+                                    send.setText("Send Friend Request");
+                                    decline.setVisibility(View.VISIBLE);
+                                }
+                                else {
+                                    Toast.makeText(getContext(), "Incomplete \n" + task.getException() , Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }
+                    else {
+                        Toast.makeText(getContext(), "Incomplete \n" + task.getException() , Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+
+        if (currentState.equals("He_sent_pending")){
+            requestRef.child(value).removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull @NotNull Task<Void> task) {
+                    if (task.isSuccessful()){
+
+                        HashMap hashMap = new HashMap();
+                        hashMap.put("status", "Friend_Pending");
+                        hashMap.put("userId", token);
+
+                        friendRequestRef.child(token).updateChildren(hashMap).addOnCompleteListener(new OnCompleteListener() {
+                            @Override
+                            public void onComplete(@NonNull @NotNull Task task) {
+                                if (task.isSuccessful()){
+                                    HashMap hashMap = new HashMap();
+                                    hashMap.put("status", "Friend_Pending");
+                                    hashMap.put("userId", value);
+                                    requestRef.child(value).updateChildren(hashMap).addOnCompleteListener(new OnCompleteListener() {
+                                        @Override
+                                        public void onComplete(@NonNull @NotNull Task task) {
+                                            if (task.isSuccessful()){
+                                                currentState = "watting_accept_friend";
+                                                send.setText("Accept");
+                                                decline.setText("Cancel");
+                                                decline.setVisibility(View.VISIBLE);
+                                            }
+                                        }
+                                    });
+                                }
+                            }
+                        });
+                    }
+                    else {
+                        Toast.makeText(getContext(), "Incomplete \n" + task.getException() , Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+
+        if (currentState.equals("watting_accept_friend")){
+            //
+        }
     }
 }
