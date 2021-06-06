12fc1cf68c4c6ce22008d35c590d2ea648ede963
RakiticVo <thienb1704852@gmail.com>
2021-06-06 16:50:52 +0700

    request_permission

--- app/src/main/java/com/example/pet/mode/activities/MainActivity.java
+++ app/src/main/java/com/example/pet/mode/activities/MainActivity.java
@@ -1,23 +1,83 @@
 package com.example.pet.mode.activities;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
 
 import com.example.pet.R;
 
+import org.jetbrains.annotations.NotNull;
+
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_CODE = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Handler().postDelayed(() -> {
+
+        if (
+                ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.SEND_SMS) +
+                ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                            Manifest.permission.SEND_SMS) ||
+                    ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setPositiveButton("", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ActivityCompat.requestPermissions(
+                                MainActivity.this,
+                                new String[]{
+                                        Manifest.permission.SEND_SMS,
+                                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                                },
+                                REQUEST_CODE
+                        );
+                    }
+                });
+                builder.setNegativeButton("", null);
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }else {
+                ActivityCompat.requestPermissions(
+                        MainActivity.this,
+                        new String[]{
+                                Manifest.permission.SEND_SMS,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE
+                        },
+                        REQUEST_CODE
+                );
+
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull @NotNull String[] permissions, @NonNull @NotNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE
+            && grantResults.length>0
+            && grantResults[0] + grantResults[1] == PackageManager.PERMISSION_GRANTED){
             Intent intent = new Intent(MainActivity.this, MainPage.class);
             startActivity(intent);
             finish();
-        },1000);
+        }
     }
 }
